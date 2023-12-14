cd2980c4de068ef11db9a8111cff56aabf3681ed
Lizaveta Shelianhouskaya <shellizaveta@yandex-team.ru>
2023-12-14 17:15:04 +0300

    feat: add hw01-gradle

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+
+plugins {
+    idea
+    id("io.spring.dependency-management")
+    id("name.remal.sonarlint") apply false
+    id("com.diffplug.spotless") apply false
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+
+allprojects {
+    group = "ru.otus"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    val testcontainersBom: String by project
+    val protobufBom: String by project
+    val guava: String by project
+
+    apply(plugin = "io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            dependency("com.google.guava:guava:$guava")
+        }
+    }
+}
+
+subprojects {
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+
+    apply<name.remal.gradle_plugins.sonarlint.SonarLintPlugin>()
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            palantirJavaFormat("2.38.0")
+        }
+    }
+}
+
+tasks {
+    val managedVersions by registering {
+        doLast {
+            project.extensions.getByType<DependencyManagementExtension>()
+                    .managedVersions
+                    .toSortedMap()
+                    .map { "${it.key}:${it.value}" }
+                    .forEach(::println)
+        }
+    }
+}
