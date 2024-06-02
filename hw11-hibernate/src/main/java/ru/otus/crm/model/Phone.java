fc8f7390710aafffe2f230cfb11595727fc5b58d
Lizaveta Shelianhouskaya <shellizaveta@yandex-team.ru>
2024-06-02 21:28:14 +0300

    feat: add hw11-hibernate

---
+++ hw11-hibernate/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,38 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "phone")
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+}
